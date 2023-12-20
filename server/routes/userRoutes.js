4f957556bdedeb18bfa7c7420f7de0c0b4c63fb7
JuzzThyne <justinepodiotan19@gmail.com>
2023-12-21 01:03:48 +0800

    add pages in user info

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { User } from "../models/userModel.js";
+import { Admin } from "../models/adminModel.js";
 import verifyToken from "../auth/authMiddleware.js";
 // import enforceHttps from "../auth/httpMiddleware.js";
 
@@ -10,22 +11,23 @@ const router = express.Router();
 // Protected route
 router.get('/', verifyToken, async (request, response) => {
     try {
-        // Access the decoded user information from the request object
         const user = request.user;
-
-        // Your route logic here (e.g., fetching data from the database)
-        const users = await User.find({});
-
+        const page = parseInt(request.query.page) || 1;
+        const limit = 20; // Set the limit to 20 per page
+        const skip = (page - 1) * limit;
+        const users = await User.find({}).skip(skip).limit(limit);
         return response.status(200).json({
-            count: users.length,
             data: users,
-            user // You can include the user information in the response if needed
+            user,
+            currentPage: page,
+            totalPages: Math.ceil(users.length / limit)
         });
     } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message });
     }
 });
+
 
 // Your existing route for adding users
 router.post('/', verifyToken, async (request, response) => {
