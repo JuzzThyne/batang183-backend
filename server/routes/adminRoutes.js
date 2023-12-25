1e781fb17f878e41d0bb18d028988c750d8ae587
JuzzThyne <justinepodiotan19@gmail.com>
2023-12-25 18:09:39 +0800

    upadte session

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -9,12 +9,12 @@ import session from "express-session";
 import ConnectMongoDBSession from "connect-mongodb-session";
 import { v4 as uuidv4 } from 'uuid'; // Import the uuid library
 import dotenv from 'dotenv';
+import jwt from "jsonwebtoken";
+import { Admin } from "../models/adminModel.js";
+import bcrypt from "bcrypt";
+
 dotenv.config();
 
-
-
-
-const router = express.Router();
 // Set up MongoDB session store
 const MongoDBStore = ConnectMongoDBSession(session);
 const store = new MongoDBStore({
@@ -29,6 +29,8 @@ const store = new MongoDBStore({
 store.on('error', (error) => {
   console.error(error);
 });
+
+const router = express.Router();
 
 router.use(session({
   genid: (req) => {
@@ -46,7 +48,42 @@ router.use(session({
   },
 }));
 
-router.post("/login", loginController);
+router.post("/login", async (req, res) => {
+  try {
+      const { username, password } = req.body;
+
+      // Check if username and password are provided
+      if (!username || !password) {
+          return res.status(400).json({ message: "Username and password are required" });
+      }
+
+      // Find the admin by username
+      const admin = await Admin.findOne({ username }).select('password');
+
+      // If the admin is not found, return an error
+      if (!admin) {
+          return res.status(401).json({ message: "Invalid username or password" });
+      }
+
+      // Compare the provided password with the hashed password in the database
+      const passwordMatch = await bcrypt.compare(password, admin.password);
+
+      // If passwords match, create a session and return a success message
+      if (passwordMatch) {
+          const token = jwt.sign({ adminId: admin._id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+          req.session.auth = true;
+          req.session.adminId = admin._id; // Optionally, store additional data in the session
+          return res.json({ success: true, message: "Login successful", token });
+      } else {
+          // If passwords do not match, return an error
+          return res.status(401).json({ message: "Invalid username or password" });
+      }
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 router.post("/logout",verifyToken, logoutController);
 router.post("/register", registerController);
 router.get("/check-session", verifyToken, (req, res) => {
