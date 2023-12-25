8a8bd6ad03d3512a50f636ef01f0ea9f6b6e4647
JuzzThyne <justinepodiotan19@gmail.com>
2023-12-25 20:10:35 +0800

    modify sending cookies

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -37,10 +37,11 @@ router.use(session({
     return uuidv4(); // Generate a new UUID for each session
   },
   secret: 'keyboard cat',
-  resave: true,
+  resave: false,
   saveUninitialized: false,
   store: store, // Use the MongoDB session store
-  cookie: { maxAge: 3600000, sameSite: null, secure: false, httpOnly: true, credentials: true  }, // 1 hour
+  // cookie: { maxAge: 3600000, sameSite: null, secure: false, httpOnly: true, credentials: true  }, // 1 hour
+  cookie: { maxAge: 3600000, httpOnly: true, }, // 1 hour
   // cookie: {
   //   maxAge: 1000 * 60 * 60 * 24, // Set the cookie to expire after 24 hours
   //   // httpOnly: true, // Ensures the cookie is only accessible through HTTP(S) requests
