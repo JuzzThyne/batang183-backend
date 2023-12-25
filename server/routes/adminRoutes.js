ae4f359102ffa04c368df557f246af73ffa14adb
JuzzThyne <justinepodiotan19@gmail.com>
2023-12-25 20:26:53 +0800

    test in localhost

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -41,7 +41,7 @@ router.use(session({
   saveUninitialized: false,
   store: store, // Use the MongoDB session store
   // cookie: { maxAge: 3600000, sameSite: null, secure: false, httpOnly: true, credentials: true  }, // 1 hour
-  cookie: { maxAge: 3600000, httpOnly: true, }, // 1 hour
+  cookie: { maxAge: 3600000, httpOnly: true, domain: 'http://localhost:5173'  }, // 1 hour
   // cookie: {
   //   maxAge: 1000 * 60 * 60 * 24, // Set the cookie to expire after 24 hours
   //   // httpOnly: true, // Ensures the cookie is only accessible through HTTP(S) requests
