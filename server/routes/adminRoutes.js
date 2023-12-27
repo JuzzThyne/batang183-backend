d0abeedd307fd2de569f1afa150b5fdd424d7a27
JuzzThyne <justinepodiotan19@gmail.com>
2023-12-27 21:29:29 +0800

    add middleware

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,25 +1,52 @@
-import express from "express";
-import { loginController, logoutController, registerController } from "../controllers/authController.js";
-import verifyToken from "../Auth/verifyToken.js";
+// authRoutes.js
+
+import express from 'express';
+import { loginController, logoutController, registerController } from '../controllers/authController.js';
+import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const router = express.Router();
 
 router.use(express.json());
 
-router.post("/login", loginController);
+// Middleware for verifying the token
+const verifyToken = (req, res, next) => {
+  // Extract the token from the request headers
+  const token = req.headers.authorization;
 
-router.post("/logout", logoutController);
+  // Check if the token is present
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized: No token provided' });
+  }
 
-router.post("/register", registerController);
+  // Verify the token
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+    }
 
-router.get("/check-session", verifyToken, (req, res) => {
+    // Attach the decoded user information to the request object
+    req.user = decoded;
+    next();
+  });
+};
+
+// Routes
+router.post('/login', loginController);
+
+router.post('/logout', verifyToken, logoutController);
+
+router.post('/register', registerController);
+
+// Protected route that requires a valid token
+router.get('/check-session', verifyToken, (req, res) => {
   // Check the authentication using the token
   if (req.user) {
     res.json({ authenticated: true, adminId: req.user.adminId });
   } else {
-    res.json({ authenticated: false, message: "Not true" });
+    res.json({ authenticated: false, message: 'Not true' });
   }
 });
 
